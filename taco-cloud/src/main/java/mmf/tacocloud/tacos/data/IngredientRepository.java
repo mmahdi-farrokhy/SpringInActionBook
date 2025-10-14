a740a86d035d15a31576cf38dd4bc84f73236129
m.farokhi <m.farokhi@behpardaz.net>
2025-10-14 17:26:40 +0330

    chapter 3 - extended CurdRepository in OrderRepository and IngredientRepository

--- taco-cloud/src/main/java/mmf/tacocloud/tacos/data/IngredientRepository.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/data/IngredientRepository.java
@@ -1,13 +1,7 @@
 package mmf.tacocloud.tacos.data;
 
 import mmf.tacocloud.tacos.Ingredient;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-public interface IngredientRepository {
-    Iterable<Ingredient> findAll();
-
-    Optional<Ingredient> findById(long id);
-
-    Ingredient save(Ingredient ingredient);
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 }
--- taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
@@ -1,7 +1,7 @@
 package mmf.tacocloud.tacos.data;
 
 import mmf.tacocloud.tacos.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
-    TacoOrder save(TacoOrder order);
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 }
