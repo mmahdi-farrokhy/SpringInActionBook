46d3b3a527b69277245ba74d2a485865f39c1aa2
m.farokhi <m.farokhi@behpardaz.net>
2025-10-15 15:54:19 +0330

    chapter 3 - added method findByDeliveryZipAndPlacedAtBetween() to OrderRepository

--- taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
@@ -3,8 +3,13 @@ package mmf.tacocloud.tacos.data;
 import mmf.tacocloud.tacos.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> readByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    List<TacoOrder> findByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 }
