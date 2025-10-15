4d788871eede9a9534dc3be39719a6ac594af980
m.farokhi <m.farokhi@behpardaz.net>
2025-10-15 16:06:21 +0330

    chapter 3 - changed table name Taco_Cloud_Order to TacoOrder

--- taco-cloud/src/main/java/mmf/tacocloud/tacos/TacoOrder.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/TacoOrder.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 @Data
-@Entity(name = "Taco_Cloud_Order")
+@Entity
 public class TacoOrder implements Serializable {
     private static final long serialVersionUID = 1L;
 
