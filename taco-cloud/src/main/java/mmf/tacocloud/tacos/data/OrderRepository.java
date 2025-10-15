ac9076386ccff6c86217a17dc32ea529311c66a5
m.farokhi <m.farokhi@behpardaz.net>
2025-10-15 15:14:20 +0330

    chapter 3 - added method findByDeliveryZip() to OrderRepository

--- taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
@@ -3,5 +3,8 @@ package mmf.tacocloud.tacos.data;
 import mmf.tacocloud.tacos.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
 }
