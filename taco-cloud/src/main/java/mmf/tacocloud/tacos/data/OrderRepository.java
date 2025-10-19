6badfea87e00ef83d87fd6a422f9a75a89398757
mmahdi-farrokhy-BPJ <m.farokhi@behpardaz.net>
2025-10-19 22:38:29 +0330

    chapter 4 - changed id type in OrderRepository

--- taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
@@ -3,7 +3,5 @@ package mmf.tacocloud.tacos.data;
 import mmf.tacocloud.tacos.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.UUID;
-
-public interface OrderRepository extends CrudRepository<TacoOrder, UUID> {
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
 }
