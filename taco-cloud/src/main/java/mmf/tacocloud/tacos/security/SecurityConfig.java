3f83913ec25d69e5eb339915f77f3820357f1a5e
mmahdi-farrokhy-BPJ <m.farokhi@behpardaz.net>
2025-10-21 20:32:58 +0330

    chapter 5 - configured bean type UserDetailsService

--- taco-cloud/src/main/java/mmf/tacocloud/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package mmf.tacocloud.tacos.security;
 
+import mmf.tacocloud.tacos.data.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -31,5 +33,17 @@ public class SecurityConfig {
                 Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
 
         return new InMemoryUserDetailsManager(usersList);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            mmf.tacocloud.tacos.User user = userRepository.findByUsername(username);
+            if (user != null) {
+                return user;
+            }
+
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
     }
 }
