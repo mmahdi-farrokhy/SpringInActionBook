54d449a365ca938bd4b067e5e3d57bdf65effc7a
m.farokhi <m.farokhi@behpardaz.net>
2025-11-09 15:04:34 +0330

    chapter 5 - implemented method getOrder()

--- taco-cloud/src/main/java/mmf/tacocloud/tacos/web/OrderAdminService.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/web/OrderAdminService.java
@@ -1,8 +1,12 @@
 package mmf.tacocloud.tacos.web;
 
+import mmf.tacocloud.tacos.TacoOrder;
 import mmf.tacocloud.tacos.data.OrderRepository;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class OrderAdminService {
@@ -15,5 +19,16 @@ public class OrderAdminService {
     @PreAuthorize("hasRole('ADMIN')")
     public void deleteAllOrders() {
         orderRepository.deleteAll();
+    }
+
+    @PostAuthorize("hasRole('ADMIN') || returnObject.user.userName == authentication.name")
+    public TacoOrder getOrder(Long id) {
+        Optional<TacoOrder> tacoById = orderRepository.findById(id);
+
+        if (tacoById.isEmpty()) {
+            throw new RuntimeException("Order not found");
+        }
+
+        return tacoById.get();
     }
 }
