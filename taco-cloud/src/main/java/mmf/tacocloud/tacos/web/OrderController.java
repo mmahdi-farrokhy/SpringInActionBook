c356eee7f8e2adb3c77368c2872f3aca813a4379
m.farokhi <m.farokhi@behpardaz.net>
2025-11-09 15:14:11 +0330

    chapter 5 - get logged in username and set it to the taco order being processed

--- taco-cloud/src/main/java/mmf/tacocloud/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/web/OrderController.java
@@ -3,7 +3,9 @@ package mmf.tacocloud.tacos.web;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import mmf.tacocloud.tacos.TacoOrder;
+import mmf.tacocloud.tacos.User;
 import mmf.tacocloud.tacos.data.OrderRepository;
+import mmf.tacocloud.tacos.data.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +14,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
+import java.security.Principal;
+
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    private final UserRepository userRepository;
     private OrderRepository orderRepository;
 
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository, UserRepository userRepository) {
         this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/current")
@@ -30,11 +36,13 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid TacoOrder tacoOrder, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder tacoOrder, Errors errors, SessionStatus sessionStatus, Principal principal) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
+        User user = userRepository.findByUsername(principal.getName());
+        tacoOrder.setUser(user);
         log.info("Order submitted {}", tacoOrder);
         orderRepository.save(tacoOrder);
         sessionStatus.setComplete();
