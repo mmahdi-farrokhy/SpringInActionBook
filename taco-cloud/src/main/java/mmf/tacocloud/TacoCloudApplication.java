76079308ce9b947085bb68f1feb106383ea665d1
m.farokhi <m.farokhi@behpardaz.net>
2025-11-10 15:33:02 +0330

    chapter 6 - created CommandLineRunner bean for 'dev' and 'qa' profiles

--- taco-cloud/src/main/java/mmf/tacocloud/TacoCloudApplication.java
+++ taco-cloud/src/main/java/mmf/tacocloud/TacoCloudApplication.java
@@ -17,7 +17,7 @@ public class TacoCloudApplication {
     }
 
     @Bean
-    @Profile("dev")
+    @Profile({"dev", "qa"})
     public CommandLineRunner dataLoader(IngredientRepository repo) {
         return args -> {
             repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
