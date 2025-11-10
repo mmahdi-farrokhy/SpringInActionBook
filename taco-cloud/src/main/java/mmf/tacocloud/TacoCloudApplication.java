7c53036d196f157ad92e6de67e8695176d08a9e9
m.farokhi <m.farokhi@behpardaz.net>
2025-11-10 15:32:29 +0330

    chapter 6 - created CommandLineRunner bean only for 'dev' profile

--- taco-cloud/src/main/java/mmf/tacocloud/TacoCloudApplication.java
+++ taco-cloud/src/main/java/mmf/tacocloud/TacoCloudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -16,6 +17,7 @@ public class TacoCloudApplication {
     }
 
     @Bean
+    @Profile("dev")
     public CommandLineRunner dataLoader(IngredientRepository repo) {
         return args -> {
             repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
