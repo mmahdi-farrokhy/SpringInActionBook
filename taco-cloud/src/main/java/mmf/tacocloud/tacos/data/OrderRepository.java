4b168bfbcf5b706fe20aac658c36539598ae7033
m.farokhi <m.farokhi@behpardaz.net>
2025-11-10 10:56:44 +0330

    chapter 6 - added new method findByUserOrderByPlacedAtDesc(user) to OrderRepository

--- taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
+++ taco-cloud/src/main/java/mmf/tacocloud/tacos/data/OrderRepository.java
@@ -1,7 +1,11 @@
 package mmf.tacocloud.tacos.data;
 
 import mmf.tacocloud.tacos.TacoOrder;
+import mmf.tacocloud.tacos.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user);
 }
