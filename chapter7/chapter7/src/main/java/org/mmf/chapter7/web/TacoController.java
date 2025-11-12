488fcc297eb5bda95c607d53707b1bbb894e8382
m.farokhi <m.farokhi@behpardaz.net>
2025-11-12 15:17:52 +0330

    implemented method tacoById()

--- chapter7/chapter7/src/main/java/org/mmf/chapter7/web/TacoController.java
+++ chapter7/chapter7/src/main/java/org/mmf/chapter7/web/TacoController.java
@@ -4,10 +4,9 @@ import org.mmf.chapter7.Taco;
 import org.mmf.chapter7.data.TacoRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos", produces = {"application/json", "text/xml"})
@@ -23,5 +22,10 @@ public class TacoController {
     public Iterable<Taco> recentTacos() {
         PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
         return tacoRepository.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Taco> tacoById(@PathVariable Integer id) {
+        return tacoRepository.findById(id);
     }
 }
