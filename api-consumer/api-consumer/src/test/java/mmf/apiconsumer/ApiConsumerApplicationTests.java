c8620e1d658c3ba660625fb881791d86b6122ca6
m.farokhi <m.farokhi@behpardaz.net>
2025-11-17 15:09:54 +0330

    project files

---
+++ api-consumer/api-consumer/src/test/java/mmf/apiconsumer/ApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package mmf.apiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
