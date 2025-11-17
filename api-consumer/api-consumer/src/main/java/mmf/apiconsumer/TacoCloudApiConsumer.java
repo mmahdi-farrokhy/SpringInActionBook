90094372cd4cc388b8b1b3aa87060c98fb94eab1
m.farokhi <m.farokhi@behpardaz.net>
2025-11-17 10:07:25 +0330

    refactored method getIngredientById() to use uri variables

--- api-consumer/api-consumer/src/main/java/mmf/apiconsumer/TacoCloudApiConsumer.java
+++ api-consumer/api-consumer/src/main/java/mmf/apiconsumer/TacoCloudApiConsumer.java
@@ -3,6 +3,9 @@ package mmf.apiconsumer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class TacoCloudApiConsumer {
     private final RestTemplate restTemplate;
@@ -12,9 +15,11 @@ public class TacoCloudApiConsumer {
     }
 
     public Ingredient getIngredientById(String ingredientId) {
+        Map<String, String> uriVariables = new HashMap<String, String>();
+        uriVariables.put("id", ingredientId);
         return restTemplate
                 .getForObject("http://localhost:8080/ingredients/{id}",
                         Ingredient.class,
-                        ingredientId);
+                        uriVariables);
     }
 }
