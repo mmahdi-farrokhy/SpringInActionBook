b86d2831847afd33477acdb86080e45026d985c1
mmahdi-farrokhy-BPJ <m.farokhi@behpardaz.net>
2025-11-22 23:13:54 +0330

    fixing errors

--- chapter8/authorization-server/src/main/java/com/example/authorizationserver/AuthorizationServerApplication.java
+++ chapter8/authorization-server/src/main/java/com/example/authorizationserver/AuthorizationServerApplication.java
@@ -1,15 +1,12 @@
 package com.example.authorizationserver;
 
+import com.example.authorizationserver.tacos.data.User;
 import com.example.authorizationserver.tacos.data.UserRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Collections;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
@@ -22,8 +19,8 @@ public class AuthorizationServerApplication {
     @Bean
     public ApplicationRunner dataLoader(UserRepository repo, PasswordEncoder encoder) {
         return args -> {
-            User userHabuma = new User("habuma", encoder.encode("password"), Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN")));
-            User userTacochef = new User("tacochef", encoder.encode("password"), Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN")));
+            User userHabuma = new User("habuma", encoder.encode("password"), "ROLE_ADMIN");
+            User userTacochef = new User("tacochef", encoder.encode("password"), "ROLE_ADMIN");
             repo.save(userHabuma);
             repo.save(userTacochef);
         };
--- chapter8/authorization-server/src/main/java/com/example/authorizationserver/tacos/data/UserRepository.java
+++ chapter8/authorization-server/src/main/java/com/example/authorizationserver/tacos/data/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.authorizationserver.tacos.data;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.core.userdetails.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByUsername(String username);
